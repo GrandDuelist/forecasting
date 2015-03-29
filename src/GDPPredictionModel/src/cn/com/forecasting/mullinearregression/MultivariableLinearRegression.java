21766c0a9811c319e211d1633ae8f8b42a5cd928
ezhihan <ezhihan@gmail.com>
2015-03-29 22:20:11 +0800

    multivariable linear regression init

--- src/GDPPredictionModel/src/cn/com/forecasting/mullinearregression/MultivariableLinearRegression.java
+++ src/GDPPredictionModel/src/cn/com/forecasting/mullinearregression/MultivariableLinearRegression.java
@@ -1,5 +1,14 @@
 package cn.com.forecasting.mullinearregression;
 
 public class MultivariableLinearRegression {
-	
+		
+	public void resolve(double [][] X, double [] Y,int numberX,int numberData, double []coef,double judgeData
+			,double[] v){
+		int a = numberX+1;
+		double[] tempArray  = new double[a*a];
+		tempArray[a*a-1] = numberData;
+		for(int i=0;i<numberX;i++){
+			
+		}
+	}
 }
