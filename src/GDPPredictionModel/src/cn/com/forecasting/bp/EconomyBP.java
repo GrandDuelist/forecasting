41d9e50cdaaaec7b61b269e00fb973c2b4a90fe9
zhihan fang <ezhihan@gmail.com>
2015-04-10 12:15:17 +0800

    bp finished

--- src/GDPPredictionModel/src/cn/com/forecasting/bp/EconomyBP.java
+++ src/GDPPredictionModel/src/cn/com/forecasting/bp/EconomyBP.java
@@ -6,47 +6,140 @@ import cn.com.sql.pojo.EconomyPoJo;
 
 /**
  * 对BP进行封装
+ * 
  * @author zhihan
- *
+ * 
  */
 public class EconomyBP {
 	public BaseAnn bp;
 	
-	public EconomyBP(int inputSize, int hiddenSize,int outputSize){
-		this.bp = new BaseAnn(inputSize,hiddenSize,outputSize);
+	private int inputSize ;
+	private int outputSize;
+	public EconomyBP(int inputSize, int hiddenSize, int outputSize) {
+		this.bp = new BaseAnn(inputSize, hiddenSize, outputSize);
+		this.inputSize=inputSize;
+		this.outputSize=outputSize;
 	}
-	
-	//直接通过EconomyPoJo的列表进行训练
-	public void trainByEconomyList(List<EconomyPoJo> pojos, int trainTimes){
-		if(this.bp==null){
+
+	// 直接通过EconomyPoJo的列表进行训练
+	public void trainByEconomyList(List<EconomyPoJo> pojos, int trainTimes) {
+		if (this.bp == null) {
 			System.out.println("please init bp at first");
 			return;
 		}
-		//对数据进行训练
-		for(int i=0;i<trainTimes;i++){
-			for(int j=0;j<pojos.size()-1;j++){
+		// 对数据进行训练
+		for (int i = 0; i < trainTimes; i++) {
+			for (int j = 0; j < pojos.size() - 1; j++) {
 				EconomyPoJo currentPojo = pojos.get(j);
-				EconomyPoJo nextPojo = pojos.get(j+1);
+				EconomyPoJo nextPojo = pojos.get(j + 1);
 				double[] currentX = currentPojo.changeAttributeToArray();
-				double[] target =new double[1];
+				double[] target = new double[1];
 				target[0] = nextPojo.getCityGDP();
-				this.bp.train(currentX, target);	
+				this.bp.train(currentX, target);
+			}
+		}
+
+		for (int j = 0; j < pojos.size() - 1; j++) {
+
+			EconomyPoJo currentPojo = pojos.get(j);
+			EconomyPoJo nextPojo = pojos.get(j + 1);
+			double[] currentX = currentPojo.changeAttributeToArray();
+			double[] target = new double[1];
+			target[0] = nextPojo.getCityGDP();
+			double[] result = this.bp.test(currentX);
+			System.out.println("预测值 " + result[0] + "  实际值 " + target[0]+ "误差： "+ 
+					(result[0]-target[0])/target[0]);
+		}
+
+	}
+
+	/**
+	 * normalize the economy and train networks
+	 * 
+	 * @param pojos
+	 * @param trainTimes
+	 */
+	public void trainByNormalizedEconomy(List<EconomyPoJo> pojos, int trainTimes) {
+		if (this.bp == null) {
+			System.out.println("please init bp at first");
+			return;
+		}
+		
+		SpecificMath preprocessing = new SpecificMath(this.inputSize);
+		preprocessing.calculateMaxMinOfXY(pojos);
+
+		// 对数据进行训练
+		for (int i = 0; i < trainTimes; i++) {
+			for (int j = 0; j < pojos.size() - 1; j++) {
+				EconomyPoJo currentPojo = pojos.get(j);
+				EconomyPoJo nextPojo = pojos.get(j + 1);
+				double[] currentX = preprocessing.normalizeX(currentPojo);
+				double[] target = new double[1];
+				target[0] = preprocessing.normalizeY(nextPojo.getCityGDP());
+				this.bp.train(currentX, target);
 			}
 		}
 		
 		
-		for(int j=0;j<pojos.size()-1;j++){
-			
+		//训练测试集
+		for (int j = 0; j < pojos.size() - 1; j++) {
+
 			EconomyPoJo currentPojo = pojos.get(j);
-			EconomyPoJo nextPojo = pojos.get(j+1);
-			double[] currentX = currentPojo.changeAttributeToArray();
-			double[] target =new double[1];
-			target[0] = nextPojo.getCityGDP();
-			double [] result = this.bp.test(currentX);
-			System.out.println("预测值 "+ result[0] + "  实际值 "+target[0] );
+			EconomyPoJo nextPojo = pojos.get(j + 1);
+			double[] currentX = preprocessing.normalizeX(currentPojo);
+			double[] target = new double[1];
+			target[0] = preprocessing.normalizeY(nextPojo.getCityGDP());
+			double[] result = this.bp.test(currentX);
+			System.out.println("预测值 " + preprocessing.reverseY(result[0]) + "  实际值 " + nextPojo.getCityGDP() + "误差： 百分之"+ 
+			100*Math.abs((preprocessing.reverseY(result[0])-nextPojo.getCityGDP())/nextPojo.getCityGDP()));
 		}
-	
+
 	}
+	
+	
+	/**
+	 * seperate normalize the economy and train networks
+	 * 
+	 * @param pojos
+	 * @param trainTimes
+	 */
+	public void trainByNormalizedEconomy(List<EconomyPoJo> pojos,List<EconomyPoJo> test, int trainTimes) {
+		if (this.bp == null) {
+			System.out.println("please init bp at first");
+			return;
+		}
+		
+		SpecificMath preprocessing = new SpecificMath(this.inputSize);
+		preprocessing.calculateMaxMinOfXY(pojos);
+
+		// 对数据进行训练
+		for (int i = 0; i < trainTimes; i++) {
+			for (int j = 0; j < pojos.size() - 1; j++) {
+				EconomyPoJo currentPojo = pojos.get(j);
+				EconomyPoJo nextPojo = pojos.get(j + 1);
+				double[] currentX = preprocessing.normalizeX(currentPojo);
+				double[] target = new double[1];
+				target[0] = preprocessing.normalizeY(nextPojo.getCityGDP());
+				this.bp.train(currentX, target);
+			}
+		}
+		
+		
+		//训练测试集
+		for (int j = 0; j < test.size() - 1; j++) {
+
+			EconomyPoJo currentPojo = test.get(j);
+			EconomyPoJo nextPojo = test.get(j + 1);
+			double[] currentX = preprocessing.normalizeX(currentPojo);
+			double[] target = new double[1];
+			target[0] = preprocessing.normalizeY(nextPojo.getCityGDP());
+			double[] result = this.bp.test(currentX);
+			System.out.println("预测值 " + preprocessing.reverseY(result[0]) + "  实际值 " + nextPojo.getCityGDP()+  "误差： 百分之"+ 
+					100*Math.abs((preprocessing.reverseY(result[0])-nextPojo.getCityGDP())/nextPojo.getCityGDP()));
+		}
+
+	}
+	
 	
 
 }
--- src/GDPPredictionModel/src/cn/com/test/BaseAnnTest.java
+++ src/GDPPredictionModel/src/cn/com/test/BaseAnnTest.java
@@ -43,7 +43,10 @@ public class BaseAnnTest {
 			handle.close();
 			
 			EconomyBP bp = new EconomyBP(8,4,1);
-			bp.trainByEconomyList(pojos, 200);
+		//	bp.trainByEconomyList(pojos, 200);
+			bp.trainByNormalizedEconomy(pojos, 50000);
+			System.out.println();
+			bp.trainByNormalizedEconomy(pojos.subList(0,pojos.size()-2),pojos.subList(pojos.size()-3, pojos.size()),50000);
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
