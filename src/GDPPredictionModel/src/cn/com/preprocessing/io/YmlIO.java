a9a4fcc190d8e1662e453feadb772729a12a6fc1
Zhihan Fang <ezhihan@gmail.com>
2015-06-13 01:50:58 +0800

    yml io init

---
+++ src/GDPPredictionModel/src/cn/com/preprocessing/io/YmlIO.java
@@ -0,0 +1,36 @@
+package cn.com.preprocessing.io;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class YmlIO {
+	private String filePath;
+	private String ymlContent;
+	
+	public YmlIO(){
+	}
+	public YmlIO(String filePath){
+		this.filePath = filePath;
+	}
+	public void readYmlFile() throws IOException
+	{
+		if(this.filePath == null){
+			new NullPointerException("file path is null");
+		}else{
+			File file = new File(filePath);
+			Long fileLength =  file.length();
+			byte[] fileContent = new byte[fileLength.intValue()];
+			FileInputStream in  = new FileInputStream(file);
+			in.read(fileContent);
+			this.ymlContent = new String(fileContent,"UTF-8");
+			}
+	}
+	
+	public void parseFromYmpToMap(){
+		
+	}
+	
+}
--- src/GDPPredictionModel/src/cn/com/test/YamlTest.java
+++ src/GDPPredictionModel/src/cn/com/test/YamlTest.java
@@ -1,12 +1,33 @@
 package cn.com.test;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Map;
+
 import org.yaml.snakeyaml.Yaml;
 
 public class YamlTest {
 	public static void main(String args[]) {
-		Yaml yaml = new Yaml();
+		/*Yaml yaml = new Yaml();
 		String document = "  a: 1\n  b:\n    c: 3\n    d: 4\n";
 		System.out.println(document);
 		System.out.println(yaml.dump(yaml.load(document)));
-	}
+*/	
+		 InputStream input;
+		try {
+			input = new FileInputStream(new File(
+			            "test_data/test.yml"));
+			Yaml yaml = new Yaml();
+			Map<String, Object> object = (Map<String, Object>) yaml.load(input);
+		    System.out.println(object);
+		    
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		    
+		}
+	
 }
