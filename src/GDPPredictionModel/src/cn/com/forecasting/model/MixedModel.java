01d6b0201d1dd4ab28522a33dbe55fdce97fc0d8
Zhihan <zhihan.fang@rutgers.edu>
2015-06-13 21:14:43 +0800

    mixed model finshed

--- src/GDPPredictionModel/src/cn/com/forecasting/model/MixedModel.java
+++ src/GDPPredictionModel/src/cn/com/forecasting/model/MixedModel.java
@@ -1,5 +1,6 @@
 package cn.com.forecasting.model;
 
+import cn.com.forecasing.type.ModelType;
 import cn.com.preprocessing.math.ImprovedKNN;
 
 /**
@@ -8,10 +9,35 @@ import cn.com.preprocessing.math.ImprovedKNN;
  *
  */
 public class MixedModel {
-	public ImprovedKNN improvateKNN = new ImprovedKNN();
+	public ImprovedKNN improvedKNN = new ImprovedKNN();
+	
+	private double[] bpResultLastYear;
+	private double[] regResultLastYear;
+	private double[] bpErrorLastYear;
+	private double[] regErrorLastYear;
 	
 	private double[] bpResult;
 	private double[] regResult;
 	private double[] bpError;
 	private double[] regError;
+	
+	private int predictionLength = 12; //预测的时间长度
+	private int predictionMonth; // 预测月份
+	private double[] mixedResult; //混合模型的预测结果
+	
+	
+	public double getPredictionResult(){
+		improvedKNN.bpErrorCurrentYear =bpError;
+		improvedKNN.bpErrorLastYear =this.bpErrorLastYear;
+		improvedKNN.regErrorLastYear = this.regErrorLastYear;
+		improvedKNN.regErrorCurrentYear = this.regError;
+		
+		ModelType type = improvedKNN.KnnVote();
+		if(type == ModelType.BP) return this.bpResult[this.predictionMonth-1];
+		else return this.regResult[this.predictionMonth-1];
+	}
+	
+	
+	
+	
 }
